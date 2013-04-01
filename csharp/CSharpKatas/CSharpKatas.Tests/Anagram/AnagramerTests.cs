fbc21cdb9f72f1325f0572bb42a5e37182b2b25b
kotsaris <konstantinidis.kostas@gmail.com>
2013-04-01 01:14:46 +0100

    completed the anagram kata

---
+++ csharp/CSharpKatas/CSharpKatas.Tests/Anagram/AnagramerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpKatas.Anagram;
+using Xunit;
+using Xunit.Extensions;
+
+namespace CSharpKatas.Tests.Anagram
+{
+    public class AnagramerTests
+    {
+        public class Generate
+        {
+            [Fact]
+            public void ShouldAnagramCorrectly()
+            {
+                var sut = new Anagramer();
+                var value = "biro";
+
+                var result = sut.Generate(value).OrderBy(x => x);
+                var expected = TakeExerciseExpectedResult().OrderBy(x => x);
+                Assert.Equal(result, expected);
+            }
+
+            private string[] TakeExerciseExpectedResult()
+            {
+                return ("biro bior brio broi boir bori" +
+                " ibro ibor irbo irob iobr iorb" +
+                " rbio rboi ribo riob roib robi" +
+                " obir obri oibr oirb orbi orib").Split(' ').ToArray();
+            }
+        }
+    }
+}
---
+++ csharp/CSharpKatas/CSharpKatas/Anagram/Anagramer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpKatas.Anagram
+{
+    public class Anagramer
+    {
+        public string[] Generate(string word)
+        {
+            var chars = word.ToList();
+            return Anagram(chars).ToArray();
+        }
+
+        private List<string> Anagram(List<char> chars)
+        {
+            var result = new List<string>();
+            if (chars.Count() == 1)
+                result.Add(chars.First().ToString());
+
+            for (var i = 0; i < chars.Count(); i++)
+            {
+                var i1 = i;
+                var subs = Anagram(chars.Where((item, index) => index != i1).ToList());
+                result.AddRange(subs.Select(sub => chars.ElementAt(i) + sub));
+            }
+
+            return result;
+        }
+    }
+}
